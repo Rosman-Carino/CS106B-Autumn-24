0c2d6e2573e1e2a99c8ddaa420e1aa86c9af96bb
Rosman Carino <rockycrn@gmail.com>
2024-10-10 17:34:49 -0700

    Adding reference and version 1 of friendsMap problem

--- Section Solution Code 1/section1_starter/friendlist.cpp
+++ Section Solution Code 1/section1_starter/friendlist.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include "map.h"
 #include "vector.h"
+#include "set.h"
 #include "filelib.h"
 #include "strlib.h"
 #include "testing/SimpleTest.h"
@@ -29,17 +30,82 @@ using namespace std;
  * by a single space. You do not have to worry about malformed entries.
  */
 
-Map<string, Vector<string>> friendList(string filename){
-    return {};
+Map<string, Set<string>> friendListReference(string filename){
+    // Step 1: Read the file and populate it into a Vector of lines
+    ifstream in;
+    Vector<string> lines;
+    if (openFile(in, filename)) { // openFile is similar to Python's open()
+        lines = readLines(in);
+    }
+    // Step 2 Declare our Map where
+    // Key: Person, Value: Set of Friends(string)
+    Map<string, Set<string>> friendsMap;
+
+    // Step 3: Iterate through 'lines;
+    for (string line : lines) {
+        // Need to call stringSplit() to tokenize each string by the
+        // delimiter/seperator which is a space " "
+        Vector<string> people = stringSplit(line, " ");
+        string person1 = people[0];
+        string person2 = people[1];
+        // We are getting a reference to the address of the friendsMap
+        // This not is for folks that have taken CS106A. We teach them this
+        // when dealing with an ADT as their value.
+        Set<string> &person1Friends = friendsMap[person1];
+        Set<string> &person2Friends = friendsMap[person2];
+        person1Friends.add(person2);
+        person2Friends.add(person1);
+    }
+    return friendsMap;
 }
+
+
+Map<string, Set<string>> friendsListV1(string filename){
+    // Step 1: Read the file and populate it into a Vector of lines
+    ifstream in;
+    Vector<string> lines;
+    if (openFile(in, filename)) { // openFile is similar to Python's open()
+        lines = readLines(in);
+    }
+    // Step 2 Declare our Map where
+    // Key: Person, Value: Set of Friends(string)
+    Map<string, Set<string>> friendsMap;
+
+    // Step 3: Iterate through 'lines;
+    for (string line : lines) {
+        // Need to call stringSplit() to tokenize each string by the
+        // delimiter/seperator which is a space " "
+        Vector<string> people = stringSplit(line, " ");
+        string person1 = people[0];
+        string person2 = people[1];
+
+        // Add each person to the map - Using the Unified Approach
+        if (!friendsMap.containsKey(person1)) {
+            friendsMap[person1] = {};
+        }
+        if (!friendsMap.containsKey(person2)) {
+            friendsMap[person2] = {};
+        }
+
+        // Add the person to each person's respective friend's set.
+        // Recall Sets do not allow for duplicates
+        friendsMap[person1].add(person2);
+        friendsMap[person2].add(person1);
+    }
+    return friendsMap;
+}
+
+
+
 
 
 ///* * * * * Provided Tests Below This Point * * * * */
 
-PROVIDED_TEST("provided test case from handout"){
-    Map<string, Vector<string>> soln;
+PROVIDED_TEST("provided test case from handout for friendList") {
+    Map<string, Set<string>> soln;
     soln["Abby"] = {"Barney", "Clyde"};
     soln["Barney"] = {"Abby"};
     soln["Clyde"] = {"Abby"};
-    EXPECT_EQUAL(soln, friendList("res/buddies.txt"));
+    EXPECT_EQUAL(soln, friendsListV1("res/buddies.txt"));
 }
+
